e74ab4bfca7f1b4badd0b36260d759e6adafdba6
Ghada Chebbi <ghadachebbi@Ghadas-MacBook-Air.local>
2021-10-11 14:23:53 +0100

    Add routes for pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
 
 function App() {
-  return <>hello from app</>;
+  return (
+    <>
+      <Link to="/"></Link>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error}></Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
