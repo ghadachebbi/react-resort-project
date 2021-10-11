375e3ace6bc26e54ce93ee78143f811a0dd2af49
Ghada Chebbi <ghadachebbi@Ghadas-MacBook-Air.local>
2021-10-11 14:32:59 +0100

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
+import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Link to="/"></Link>
       <Switch>
         <Route exact path="/" component={Home} />
