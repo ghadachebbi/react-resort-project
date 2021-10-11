d1bb9dd467802a590a11cc0d29cf19ed6283ac66
Ghada Chebbi <ghadachebbi@Ghadas-MacBook-Air.local>
2021-10-11 20:06:15 +0100

    Format the data in context file

--- src/context.js
+++ src/context.js
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
+import items from "./data";
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
-  state = {};
+  state = {
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    console.log(rooms);
+  }
+
+  formatData(items) {
+    let tempItems = items.map((item) => {
+      let id = item.sys.id;
+      let images = item.fields.images.map((image) => image.fields.file.url);
+      let room = { ...item.fields, images: images, id };
+      return room;
+    });
+    return tempItems;
+  }
+
   render() {
     return (
-      <RoomContext.Provider value="hello">
-        {this.props.childen}
+      <RoomContext.Provider value={{}}>
+        {this.props.children}
       </RoomContext.Provider>
     );
   }
