31e3bd15ba75b48c6411e92b563d98591ca47830
Ghada Chebbi <ghadachebbi@Ghadas-MacBook-Air.local>
2021-10-12 20:08:23 +0100

    Display images in SingleRoom

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -41,14 +41,27 @@ export default class SingleRoom extends Component {
       pets,
       images,
     } = room;
+    const [mainImg, ...defaultImg] = images;
+    //check the rest of the images except the background image
+    //console.log(defaultImg);
+
     return (
-      <StyledHero img={images[0] || this.state.defaultBcg}>
-        <Banner title={`${name} room`}>
-          <Link to="/rooms" className="btn-primary">
-            back to rooms
-          </Link>
-        </Banner>
-      </StyledHero>
+      <>
+        <StyledHero img={mainImg || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {defaultImg.map((item, index) => {
+              return <img key={index} src={item} alt={name} />;
+            })}
+          </div>
+        </section>
+      </>
     );
   }
 }
