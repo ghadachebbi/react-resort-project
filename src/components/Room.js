fa7230da37a1380bc094f7fab38d628facb841aa
Ghada Chebbi <ghadachebbi@Ghadas-MacBook-Air.local>
2021-10-12 13:17:13 +0100

    Add price and name for featured rooms

--- src/components/Room.js
+++ src/components/Room.js
@@ -7,7 +7,15 @@ export default function Room({ room }) {
     <article className="room">
       <div className="img-container">
         <img src={images[0] || defaultImg} alt="single room" />
+        <div className="price-top">
+          <h6>${price}</h6>
+          <p>per night</p>
+        </div>
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+          Features
+        </Link>
       </div>
+      <p className="room-info">{name}</p>
     </article>
   );
 }
